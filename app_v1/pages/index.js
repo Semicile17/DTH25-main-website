160f227a4d7d829eaa6d92ac07d56f1ba9762b21
Rohit Mahant <rohitsinghmahant707@gmail.com>
2024-12-23 11:51:01 -0500

    initiated landing page

--- app_v1/pages/index.js
+++ app_v1/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,11 +14,15 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-         Initiated on 23rd December 2024 , 10:20 IST 
-         
-          </div>
+      <div className="h-screen w-full flex justify-center items-center">
+         <div className="flex flex-col ">
+            <p className="font-serif text-[12px] md:text-lg">Welcome to the</p>
+            <h1 className="md:text-7xl text-3xl font-bold">Digital Treasure Hunt</h1>
+            <p className="text-right font-serif text-[12px] md:text-lg">2025</p>
+            <div className="flex justify-center">
+            <button className="m-10 bg-gradient-to-r from-blue-300 to-blue-600 md:w-56 w-44 h-12  text-gray-700 font-bold rounded-xl ">Join the hunt</button>
+            </div>
+         </div>
+      </div>
   );
 }
